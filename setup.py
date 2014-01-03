a47816df26f10eab22b517562a0e4beb0b0a9a4e
Bryan Silverthorn <bcs@cargo-cult.org>
2014-01-03 15:28:57 -0800

    Bump version.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ requires = []
 
 setuptools.setup(
     name="pybdaq",
-    version="0.0.5",
+    version="0.0.6",
     packages=setuptools.find_packages(),
     cmdclass=cmdclass,
     ext_modules=ext_modules,
